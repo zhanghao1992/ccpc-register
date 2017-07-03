8da4828a7fc8cfa6b72732f708b0ed2423b1cd8e
songrenfei <songrf@imsa.cn>
2017-07-03 16:15:24 +0800

    init project

---
+++ ccpc-apply/src/main/java/com/zhongqi/CcpcApplyApplication.java
@@ -0,0 +1,12 @@
+package com.zhongqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpcApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcpcApplyApplication.class, args);
+	}
+}
