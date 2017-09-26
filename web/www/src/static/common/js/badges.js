0cb879831fae9e18b86a7080e83955f3057b424a
zhanghao1992 <386708307@qq.com>
2017-09-26 15:07:47 +0800

    修改cpid

--- web/www/src/static/common/js/badges.js
+++ web/www/src/static/common/js/badges.js
@@ -85,67 +85,79 @@ var cpList = [
         index: 1,
         cpName: '北京联众互动网络股份有限公司',
         QRsrc: __uri('/static/common/images/QRcodes/lzaz.png'),
-        cpId: 'e44ab68b1c7bb15fc7e014103'
+        cpId: 'e44ab68b1c7bb15fc7e01410301'
     },
     {
         index: 2,
-        cpName: '腾讯科技（深圳）有限公司',
+        cpName: '腾讯科技（深圳）有限公司（手Q）',
         QRsrc: __uri('/static/common/images/QRcodes/tx.jpg'),
-        cpId: '584a419472284b39590f88105'
+        cpId: '584a419472284b39590f8810506'
     },
     {
         index: 3,
-        cpName: '北京新浪互联信息服务有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/xlaz.png'),
-        cpId: '944f8f964634dc2bca99e8109'
+        cpName: '腾讯科技（深圳）有限公司（微信）',
+        QRsrc: __uri('/static/common/images/QRcodes/tx.jpg'),
+        cpId: '4a4f2ebd196a1692c2ca8010505'
     },
     {
         index: 4,
-        cpName: '深圳市玩呗娱乐科技有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/wb.png'),
-        cpId: '2640d6ab6fcfcd89a41d49123'
+        cpName: '北京新浪互联信息服务有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/xlaz.png'),
+        cpId: '944f8f964634dc2bca99e810902'
     },
     {
         index: 5,
-        cpName: '深圳市银溪数码技术有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/yinxi.png'),
-        cpId: '7e4e50b1166357a640ce03129'
+        cpName: '深圳市玩呗娱乐科技有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/wb.png'),
+        cpId: '2640d6ab6fcfcd89a41d4912301'
     },
     {
         index: 6,
-        cpName: '吉林风雷网络科技股份有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/flewm.png'),
-        cpId: '6a45529cedc5561cf8a215131'
+        cpName: '深圳市银溪数码技术有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/yinxi.png'),
+        cpId: '7e4e50b1166357a640ce0312901'
     },
     {
         index: 7,
-        cpName: '在线途游',
-        QRsrc: __uri('/static/common/images/QRcodes/tuyou.jpg'),
-        cpId: '344a218f47dcb9489cce69135'
+        cpName: '吉林风雷网络科技股份有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/flewm.png'),
+        cpId: '6a45529cedc5561cf8a21513101'
     },
     {
         index: 8,
-        cpName: '深圳维京人网络科技有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/qp.png'),
-        cpId: '344a218f47dcb9489cce69135'
+        cpName: '在线途游',
+        QRsrc: __uri('/static/common/images/QRcodes/tuyou.jpg'),
+        cpId: '1244dcb577b471777e613e11101'
     },
     {
         index: 9,
-        cpName: '上海波克城市网络科技股份有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/pike.png'),
-        cpId: '164fffadbec40b80d0d0cb125'
+        cpName: '深圳维京人网络科技有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/qp.png'),
+        cpId: '344a218f47dcb9489cce6913501'
     },
     {
         index: 10,
-        cpName: '北京大国小鲜文化传媒有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/dgxx.png'),
-        cpId: 'a04dc191dd72187b707158137'
+        cpName: '上海波克城市网络科技股份有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/pike.png'),
+        cpId: '164fffadbec40b80d0d0cb12501'
     },
     {
         index: 11,
+        cpName: '北京大国小鲜文化传媒有限公司',
+        QRsrc: __uri('/static/common/images/QRcodes/dgxx.png'),
+        cpId: 'a04dc191dd72187b70715813701'
+    },
+    {
+        index: 12,
         cpName: '天津中棋惟业体育发展有限公司',
-        QRsrc: __uri('/static/common/images/QRcodes/zq.png'),
-        cpId: 'e943cc98cc4c7b66708f2d107'
+        QRsrc: __uri('/static/common/images/QRcodes/zq.jpg'),
+        cpId: 'e943cc98cc4c7b66708f2d10702'
+    },
+    {
+        index: 13,
+        cpName: '北京诚誉天下科技有限公司（皮皮）',
+        QRsrc: __uri(''),
+        cpId: 'cf4cb7b12efb1501892b7e13301'
     }
 ];
 
